e8c36eebc29aff93806bc401fac19475f80bdbd5
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2018-11-10 11:40:38 -0500

    Gibbs Training Data

--- gibbs_chem_training_data.py
+++ gibbs_chem_training_data.py
@@ -26,7 +26,8 @@ if __name__=='__main__':
     _,els=run_Chempy_sample(param_grid[0],a)
     
     # Now run multiprocessing
-    p=mp.Pool()
+    cpus=mp.cpu_count
+    p=mp.Pool(min(10,cpu_count))
     output=list(tqdm.tqdm(p.imap(runner,range(N)),total=N))
     
     end_time=time.time()
